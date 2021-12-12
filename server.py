47bad54c684456cae789117094bdd0514018ec53
meepowlz <meepowlz@gmail.com>
2021-12-12 16:19:28 -0500

    Added render_template functionality

--- server.py
+++ server.py
@@ -1,19 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 port = 8080
 
 @app.route("/")
 def home_route():
-	return "Home page"
+	return render_template("base.html")
 
 @app.route("/login")
 def login_route():
-	return "Login page"
+	return render_template("login.html")
 
 @app.route("/landing")
 def landing_route():
-	return "Login Successful. Landing page"
+	return render_template("landing.html")
 
 app.run(host="localhost", port=port)
 
