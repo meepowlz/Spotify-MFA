98edb4f6ac98d78b5b78e0dd0cb206f37536f8a5
meepowlz <meepowlz@gmail.com>
2022-01-09 16:14:20 -0500

    no longer cheese!

--- server.py
+++ server.py
@@ -3,20 +3,24 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 port = 8080
 
+
 @app.route("/")
 def home_route():
 	return render_template("base.html")
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login_route():
 	if request.method == "GET":
 		return render_template("login.html")
 	print(request.form["username"], request.form["password"])
-	return "Cheese"
+	return "Login Successful"
+
 
 @app.route("/landing")
 def landing_route():
 	return render_template("landing.html")
+
 
 app.run(host="localhost", port=port)
 
