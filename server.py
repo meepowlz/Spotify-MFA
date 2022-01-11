ed16a9a9ace76a3b1ef52d5a787f7d2852cfd184
meepowlz <meepowlz@gmail.com>
2022-01-11 09:27:43 -0500

    Add code generation & start code verification

--- server.py
+++ server.py
@@ -10,6 +10,31 @@ account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
 
+# from https://www.twilio.com/docs/verify/api/verification
+# sends a verification code to the receiving number
+def sendCode():
+	verification = client.verify \
+		.services(os.getenv("VERIFICATION_SID")) \
+		.verifications \
+		.create(to=os.getenv("RECEIVING_NUM"), channel="sms")
+
+	print(verification.sid)
+
+# from https://www.twilio.com/docs/verify/api/verification-check
+# something up on line 26? these docs do not make sense
+def checkCode(code_input):
+	verification_check = client.verify \
+		.services(os.getenv("VERIFICATION_SID")) \
+		.verification_checks \
+		.create(verification_sid=os.getenv("VERIFICATION_SID"), code=code_input)
+
+	print(verification_check.status)
+
+sendCode()
+code_input = input("Enter code: ")
+checkCode(code_input)
+
+
 def send_message(message_text):
 	message = client.messages.create(
 		to=os.getenv("RECEIVING_NUM"),
@@ -17,6 +42,7 @@ def send_message(message_text):
 		body=message_text)
 
 	print(message.sid)
+
 
 app = Flask(__name__)
 port = 8080
