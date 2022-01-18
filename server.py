18d2cf5d8d8c2ff3f19513a1011a4497f2206e14
meepowlz <meepowlz@gmail.com>
2022-01-17 21:14:56 -0500

    Add Flask sessions

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
 import flask
 from flask import Flask, render_template, request, session
+from flask_session import Session
 from twilio.rest import Client
 import os
 from dotenv import load_dotenv
 
+# Allows environment variables to be accessed
 load_dotenv()
 
+
+# Twilio
+
+# Sets up Twilio client object(?) for sending verification codes
 account_sid = os.getenv("TWILIO_ACCOUNT_SID")
 auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 client = Client(account_sid, auth_token)
@@ -49,31 +55,60 @@ def send_message(message_text):
 	print(message.sid)
 
 
+# Flask application
+
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 port = 8080
+
+
+# Checks if a user is logged in or authenticated at each page
+# can this be used to minimize code footprint w/o a redirect issue?
+def check_session():
+	if not session.get("username"):
+		return flask.redirect("/login")
+	elif not session.get("verified"):
+		return flask.redirect("/authenticate")
+	else:
+		return flask.redirect("landing")
 
 
 @app.route("/")
 def home_route():
-	return flask.redirect("/login")
+	if not session.get("username"):
+		return flask.redirect("/login")
+	elif not session.get("verified"):
+		return flask.redirect("/authenticate")
+	else:
+		return flask.redirect("landing")
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login_route():
-	if request.method == "GET":
-		return render_template("login.html")
-	else:
-		send_code(request.form["mobile_number"])
+	if request.method == "POST":
+		# Saves username
+		session["username"] = request.form.get("username")
+		session["password"] = request.form.get("password") # figure out the security ramifications of this
+		# Disabled code sending until login finalized
+		# send_code(request.form["mobile_number"])
 		return flask.redirect("/authenticate")
+	return render_template("login.html")
 
 
 @app.route("/authenticate", methods=["GET", "POST"])
 def authenticate_route():
 	if request.method == "GET":
+		if not session.get("username"):
+			return flask.redirect("/login")
+		elif session.get("verified"):
+			flask.redirect("/landing")
 		return render_template("authenticate.html")
 	else:
 		verification_status = check_code(request.form["code_textbox"])
 		if verification_status:
+			session["verified"] = True
 			return flask.redirect("/landing")
 		else:
 			return render_template("authenticate.html", auth_status="Authentication failed")
@@ -81,7 +116,19 @@ def authenticate_route():
 
 @app.route("/landing")
 def landing_route():
+	if not session.get("username"):
+		return flask.redirect("/login")
+	if not session.get("verified"):
+		return flask.redirect("/authenticate") # Should it redirect here..?
 	return render_template("landing.html")
+
+
+@app.route("/logout")
+def logout_route():
+	session["username"] = None
+	session["password"] = None
+	session["verified"] = False # should this be none?
+	return flask.redirect("/")
 
 
 app.run(host="localhost", port=port)
