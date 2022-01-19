a1a48861378c91cb5f2abaf9539ecac9f61bd515
meepowlz <meepowlz@gmail.com>
2022-01-19 12:48:05 -0500

    Update example of decorators

--- decorators.py
+++ decorators.py
@@ -4,17 +4,24 @@ To aid in creating decorators for login purposes
 """
 
 
-# Making sense of decorators
-def cheese(function):
-    def wrapper():
-        print("Before the function")
-        function()
+# Cheese is the decorator
+def cheese(function):  # do_math is passed into cheese here
+    def wrapper(x, y):  # the x and y arguments from do_math are accessed by wrapper
+        print("Starting value: x=" + str(x) + " y=" + str(y))
+        result = function(x, y)  # now do_math is called
         print("After function!")
+        print("Result: " + str(result))
+
     return wrapper
 
 
 @cheese
-def our_function():
+# do_math is passed into cheese as an argument
+def do_math(x, y):  # x and y passed as arguments
     print("I'm running!")
+    print(str(x) + " is multiplied by " + str(y))
+    result = x*y
+    return result
 
-our_function()
+
+do_math(4, 7)
